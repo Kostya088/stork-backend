062f7e3ef67fc3a0e038f8c631fb171b8f221c3a
Kostya Polishchuk <kostya.polishchuk@gmail.comgit congig --get user.namegit config --global user.email kostya.polishchuk@gmail.com>
2026-04-22 23:44:58 +0200

    comment yet not existing imports

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import 'dotenv/config';
-import { logger } from './middleware/logger.js';
-import { notFoundHandler } from './middleware/notFoundHandler.js';
-import { errorHandler } from './middleware/errorHandler.js';
+// import { logger } from './middleware/logger.js';
+// import { notFoundHandler } from './middleware/notFoundHandler.js';
+// import { errorHandler } from './middleware/errorHandler.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
-import { errors } from 'celebrate';
+// import { errors } from 'celebrate';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
@@ -12,15 +12,15 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(cors());
-app.use(logger);
+// app.use(logger);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(notFoundHandler);
+// app.use(notFoundHandler);
 
-app.use(errors());
+// app.use(errors());
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
 await connectMongoDB();
 
