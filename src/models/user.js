07723c0560f4d8d7523d1037d1654a1a046c6967
lunuarovich <lenuar.max@gmail.com>
2026-04-23 19:26:21 +0300

    add user controller and user model

--- src/models/user.js
+++ src/models/user.js
@@ -3,13 +3,72 @@ import { GENDERS } from '../constants/genders.js';
 
 const userSchema = new Schema(
   {
-    name: { type: String, required: true, maxlength: 32 },
-    email: { type: String, required: true, maxlength: 64 },
-    password: { type: String, required: true },
-    gender: { type: String, enum: GENDERS },
-    dueDate: { type: Date, required: true },
-    avatar: {type: String},
-  }
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      maxlength: 64,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      maxlength: 128,
+    },
+    avatar: {
+      type: String,
+      default:
+        'https://res.cloudinary.com/dw5lx23vf/image/upload/v1776898970/leleka-app/avatars/x3pvpldi49evwdpdkgl9.jpg',
+    },
+    gender: {
+      type: String,
+      enum: GENDERS,
+      default: null,
+    },
+    dueDate: {
+      type: Date,
+      default: null,
+    },
+    theme: {
+      type: String,
+      enum: ['light', 'blue', 'pink'],
+      default: 'light',
+    },
+    pendingEmail: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+    pendingEmailToken: {
+      type: String,
+      default: null,
+    },
+    pendingEmailTokenExpires: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
 );
+
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  delete obj.pendingEmail;
+  delete obj.pendingEmailToken;
+  delete obj.pendingEmailTokenExpires;
+  return obj;
+};
 
 export const User = model('User', userSchema);
